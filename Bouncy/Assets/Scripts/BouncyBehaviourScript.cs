ed1e5513e087aa1d23c51210d6c13d43ad7f967c
Jiayi <jfu00001@ucy.ac.cy>
2018-02-22 20:03:55 +0200

    kati

---
+++ Bouncy/Assets/Scripts/BouncyBehaviourScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BouncyBehaviourScript : MonoBehaviour {
+    public int speed;
+    Vector2 movement;
+    // Use this for initialization
+    void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update() { 
+    
+
+        Rigidbody2D move = GetComponent<Rigidbody2D>();
+        movement=new Vector2();
+        if(Input.GetKey("left")){
+            movement = new Vector2(-1, 0);
+            move.AddForce (movement * speed);
+            //move.velocity = movement * speed;
+
+        }
+        else if (Input.GetKey("right"))
+        {
+            movement = new Vector2(1, 0);
+            move.AddForce(movement * speed);
+
+            //move.velocity = movement * speed;
+
+        }
+        else
+        {
+            move.AddForce(movement * speed * -1);
+            movement = new Vector2(0, 0);
+        }
+
+    }
+}

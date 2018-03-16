057a3aa2694927b535033e151c9c0bfe195e6422
avakan01 <avakan01@ucy.ac.cy>
2018-03-16 13:45:57 +0200

    Scenes: level2, homepage. Functionality: camera follows Bouncy. Prefaps: Added camera

---
+++ Bouncy/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+    public Transform cameraTrackTarget;
+
+    public float trackingSpeed;
+
+    public float cameraZDepth = -10f;
+    public float minX, minY;
+    public float maxX, maxY;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cameraTrackTarget != null)
+        {
+            var newPosition = Vector2.Lerp(transform.position, cameraTrackTarget.position, Time.deltaTime * trackingSpeed);
+            var camPosition = new Vector3(newPosition.x, newPosition.y, cameraZDepth);
+
+            var newX = Mathf.Clamp(camPosition.x, minX, maxX);
+            var newY = Mathf.Clamp(camPosition.y, minY, maxY);
+            transform.position = new Vector3(newX, newY, cameraZDepth);
+        }
+    }
+}
---
+++ Bouncy/Assets/Scripts/bounceHomepage.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bounceHomepage : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rb.velocity.y == 0)
+        {
+            rb.AddForce(new Vector2(0, 7), ForceMode2D.Impulse);
+        }
+    }
+}

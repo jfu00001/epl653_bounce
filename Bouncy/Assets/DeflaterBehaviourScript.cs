5e80ea00b86d3683797cce874191ba7344ec72cc
Aliki Gregoriou <sc14ag@leeds.ac.uk>
2018-03-27 11:13:45 +0300

    Implemented deflater behaviour

---
+++ Bouncy/Assets/DeflaterBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeflaterBehaviourScript : MonoBehaviour {
+	
+	public Transform prefab;
+
+	void Start () {}
+
+	void Update () {}
+
+	void OnCollisionEnter2D(Collision2D other){
+
+		if (other.gameObject.tag == "BouncyBig") {
+			//Instantiate first to get the position of the previous object
+			Instantiate (prefab, other.gameObject.transform.position, other.gameObject.transform.rotation);
+			Destroy (other.gameObject);
+
+		} 
+	}
+}

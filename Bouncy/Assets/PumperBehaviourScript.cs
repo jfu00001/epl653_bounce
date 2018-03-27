0c24071d9c464f8e9ce15bc7f99742c847e2c334
Aliki Gregoriou <sc14ag@leeds.ac.uk>
2018-03-27 15:15:34 +0300

    Implemented pumper behaviour

---
+++ Bouncy/Assets/PumperBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PumperBehaviourScript : MonoBehaviour {
+
+	public Transform prefab;
+	
+	void Start () {}
+	
+	void Update () {}
+	
+	void OnCollisionEnter2D(Collision2D other){
+		
+		if (other.gameObject.tag == "BouncySmall") {
+			//Instantiate first to get the position of the previous object
+			Instantiate (prefab, other.gameObject.transform.position, other.gameObject.transform.rotation);
+			Destroy (other.gameObject);
+			
+		} 
+	}
+}
